a62eb405eb64cae06746874760750a3cc996cc13
Qiyue Yao <qiyueyao@umich.edu>
2019-04-20 14:55:27 -0400

    debug

--- user/views.py
+++ user/views.py
@@ -23,9 +23,9 @@ def getuser(request, user_id):
     if request.method != 'GET':
         return HttpResponse(status=404)
     cursor = connection.cursor()
-    print("SELECT * FROM Users WHERE u_id = '" + str(user_id) + "';")
     cursor.execute("SELECT * FROM Users WHERE u_id = '" + str(user_id) + "';")
     return_data = cursor.fetchone()
+    print(return_data)
     result = {}
     result['u_id'] = return_data[0]
     result['username'] = return_data[1]
